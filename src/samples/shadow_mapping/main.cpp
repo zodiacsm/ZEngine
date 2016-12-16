8b1d60fb31205bad75f39eebf15844c4bf243dd5
mshi <mshi@bainainfo.com>
2016-12-16 18:00:43 +0800

    add bloom project

--- src/samples/shadow_mapping/main.cpp
+++ src/samples/shadow_mapping/main.cpp
@@ -223,7 +223,7 @@ int main()
         
         std::string strFrame = "";
         float2str(1.0f / renderTime, strFrame);
-        Font::getInstance()->RenderText(strFrame, 25.0f, 25.0f, 1.0f, glm::vec3(0.5, 0.8f, 0.2f));
+        Font::getInstance()->RenderText(strFrame, 25.0f, 25.0f, 0.5f, glm::vec3(0.5, 0.8f, 0.2f));
         
         Game::getInstance()->render();
         
