6ccbcbfbb671ea5b8f7b92255376d5b21fefff88
mshi <mshi@bainainfo.com>
2017-02-04 14:14:15 +0800

    make it work on mac OS X

--- src/samples/depth_testing/main.cpp
+++ src/samples/depth_testing/main.cpp
@@ -45,6 +45,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "LearnOpenGL", nullptr, nullptr); // Windowed
     glfwMakeContextCurrent(window);
